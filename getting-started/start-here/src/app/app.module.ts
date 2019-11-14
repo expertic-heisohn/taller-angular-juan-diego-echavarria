5e824e57cddeeabac6d82d3d0d0bc8b82ec9828a
camilo <camilomontoya2@gmail.com>
2019-11-14 16:25:26 -0500

    counters

--- getting-started/start-here/src/app/app.module.ts
+++ getting-started/start-here/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from "./app.component";
 import { MDBBootstrapModule } from "angular-bootstrap-md";
-import { HelloWorld } from "../components/hello-world/hello-world.component";
+import { HelloWorld } from "./components/hello-world/hello-world.component";
 
 @NgModule({
   declarations: [AppComponent, HelloWorld],
--- getting-started/start-here/src/components/hello-world/hello-world.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "hello-world",
-  templateUrl: "./hello-world.component.html"
-})
-export class HelloWorld {}
