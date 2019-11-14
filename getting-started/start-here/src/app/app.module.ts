3450679a39690b37bf521ebdf745f393ac6894c5
camilo <camilomontoya2@gmail.com>
2019-11-14 15:43:10 -0500

    creamos componente hello-world

--- getting-started/start-here/src/app/app.module.ts
+++ getting-started/start-here/src/app/app.module.ts
@@ -1,14 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { HelloWorld } from "../components/hello-world/hello-world.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent, HelloWorld],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -16,6 +14,6 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [ NO_ERRORS_SCHEMA ]
+  schemas: [NO_ERRORS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ getting-started/start-here/src/components/hello-world/hello-world.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "hello-world",
+  templateUrl: "./hello-world.component.html"
+})
+export class HelloWorld {}
